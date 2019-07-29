ec205b7a4530ab2d94e4045b8a97c80322b4c0fd
atul.kumar1 <atul.kumar1@cgi.com>
2019-07-29 12:21:38 +0530

    autowire-xml task 3 completed

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -8,7 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
-
 /**
  * Hello world!
  *
@@ -17,16 +16,10 @@ public class Main
 {
     public static void main( String[] args )
     {
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie =(Movie) context.getBean("movie");
+        System.out.println(movie);
 
-            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-            Movie movie =(Movie) context.getBean("movie");
-            System.out.println(movie);
-        Movie movie1 =(Movie) context.getBean("movie1");
-        System.out.println(movie1);
-        Movie movie2 =(Movie) context.getBean("movie");
-        System.out.println(movie2);
-        System.out.println(movie==movie1);
-
-        }
     }
+}
 
